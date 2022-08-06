67b15e666ab28c9f87a1f87533afd5f17d57f779
Yuzuru Kanda <ykdevs@ymail.ne.jp>
2022-08-06 13:50:22 +0900

    init

---
+++ src/test/java/com/ykdevs/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykdevs.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
