66c9f20131ca0eecb8dddc6cd6a6f4f981aab2d5
Yuzuru Kanda <ykdevs@ymail.ne.jp>
2022-08-15 04:15:27 +0900

    mod

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:11
 COPY  build/libs/spring-sample-0.0.1-SNAPSHOT.jar /var/myapp/spring-sample.jar
 WORKDIR /var/myapp
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","spring-sample.jar"]
--- src/main/java/com/ykdevs/springsample/controller/IndexController.java
+++ src/main/java/com/ykdevs/springsample/controller/IndexController.java
@@ -2,12 +2,34 @@ package com.ykdevs.springsample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index(ModelAndView modelview,
+                              HttpServletRequest request,
+                              @RequestHeader(name="Authorization",required=false) String authorization) {
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        String buff = "";
+        while (headerNames.hasMoreElements()) {
+
+            // ヘッダ名と値を取得
+            String headerName = (String) headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            buff += headerName + " : " + headerValue;
+        }
+
+        modelview.setViewName("index");
+        modelview.addObject("token", authorization);
+        modelview.addObject("header", buff);
+        return modelview;
     }
 }
